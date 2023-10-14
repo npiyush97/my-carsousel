0582991b9d1ce9015c929ba172cc65c45ce1659e
npiyush97 <npiyush35@gmail.com>
2023-10-14 18:44:42 +0530

    some fix

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const swiper = new Swiper('.mySwiper', {
-    // Optional parameters
     loop: true,
-  
-    // If we need pagination
+
     pagination: {
-      el: '.swiper-pagination',
+        el: '.swiper-pagination',
     },
-  
-    // Navigation arrows
+
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+
+});
